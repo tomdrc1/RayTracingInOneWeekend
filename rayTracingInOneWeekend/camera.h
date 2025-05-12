fa98609230f583634796e7c1fe32e6634ec259c6
tomdrc1 <tomdrc4@gmail.com>
2025-05-12 20:04:36 +0800

    Added defocus blur

--- rayTracingInOneWeekend/camera.h
+++ rayTracingInOneWeekend/camera.h
@@ -10,24 +10,28 @@
 #include "vec3.h"
 #include "ray.h"
 
+#define degreesToRadians(x) x * M_PI / 180.0
+
 typedef struct Camera
 {
 	Vec3 center;
-	Vec3 lookFrom;
-	Vec3 lookAt;
-	Vec3 vUp;
 
 	Vec3 pixelDeltaU;
 	Vec3 pixelDeltaV;
+
+	Vec3 defocusDiskU;
+	Vec3 defocusDiskV;
 
 	Vec3 pixel00Location;
 	unsigned int maxDepth;
 
 	double vfov;
-
+	double defocusAngle;
+	double focusDist;
 } Camera;
 
 
 void cameraInit(Camera* camera, const Vec3 lookFrom, const Vec3 lookAt, const unsigned int imageWidth, const unsigned int imageHeight);
 void cameraGenerateRay(const Camera* camera, const Vec2 PixelCoordinates, Ray* rayOut);
+Vec3 cameraDefocusDiskSample(const Camera* camera);
 void cameraDestroy(Camera* camera);
